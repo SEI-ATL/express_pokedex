e58a5e366f86278ad02dca6b4eed405c69e6d1a5
Your Name <you@example.com>
2020-11-20 19:47:18 -0500

    finish part 3 and beginning part 4

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,30 @@
 var express = require('express');
+let db = require('../models')
 var router = express.Router();
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then((favorites) => {
+    res.render('pokemon/index', { favorites })
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+// What is the sequelize function we use here?
+  // console.log(req.body.name) // add this to the database
+  db.pokemon.create({
+    name: req.body.name
+  }).then((pokemon) => {res.redirect('/pokemon')})
 });
+
+
+router.get('/pokemon')
+
+
+
+
 
 module.exports = router;
