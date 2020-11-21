e2cefce5041843c4640638219ad491f353ba7606
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-21 17:51:29 -0500

    [pokemon.js, .ejs] Passing and displaying one name

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,10 +1,19 @@
 var express = require('express');
+const db = require('../models');
 var router = express.Router();
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+ db.pokemon.findAll()
+  .then((pokes) => {
+    // console.log(`This is the console for poke: ${poke}. It is ${typeof poke}`)
+    console.log(pokes)
+    res.render('pokemon', { pokes })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
