a844f5538234fe8d2c0aa2d12ac20af497074bbe
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-22 13:19:22 -0500

    Add ability to add favoites to db and render

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios'); 
+const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,19 +12,19 @@ app.use(ejsLayouts);
 
 // GET - main index of site
 app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
-  // Use request to call the API
-  axios.get(pokemonUrl).then(response => {
-    let pokemon = response.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
-  });
+    let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
+    // Use request to call the API
+    axios.get(pokemonUrl).then(response => {
+        let pokemon = response.data.results;
+        res.render('index', { pokemon: pokemon });
+    });
 });
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
 
 const server = app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+    console.log(`Server is running on ${port}`);
 });
 
 module.exports = server;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,35 @@
+const { default: Axios } = require('axios');
 var express = require('express');
 var router = express.Router();
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+    // TODO: Get all records from the DB and render to view
+    db.pokemon.findAll().then(pokemon => {
+        res.render('pokemon', { pokemon });
+    })
+
+});
+
+router.get('/:id', function(req, res) {
+    let name = req.params.id;
+    // TODO: Get all info from the api and render to view
+    Axios.get(`http://pokeapi.co/api/v2/pokemon/${name}`).then(response => {
+        //get info, render
+
+        res.render('details', { details, name });
+    })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+    // TODO: Get form data and add a new record to DB
+    let newPokemon = req.body.name;
+    db.pokemon.findOrCreate({ where: { name: newPokemon } }).then(() => {
+        res.redirect('/pokemon');
+    })
+
 });
 
 module.exports = router;
