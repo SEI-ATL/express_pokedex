99b1f9729ed461b7aa94d146c5fbf5536f702e9b
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-22 12:53:24 -0500

    Set up database

---
+++ dbTest.js
@@ -0,0 +1,9 @@
+/*
+
+const db = require('./models')
+
+db.pokemon.findOrCreate({ where: { name: 'pikachu' } }).then(([pokemon, created]) => {
+    console.log(pokemon)
+}).catch((err) => {
+    console.log(err)
+}); */
