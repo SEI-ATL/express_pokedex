23ceb3634ac6d986543aaedd85d1594d1f859e75
jtreeves <jackson.t.reeves@gmail.com>
2020-11-22 21:59:51 -0500

    [formatting] Delete semicolons

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios'); 
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios') 
+const ejsLayouts = require('express-ejs-layouts')
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(require('morgan')('dev'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
-app.use(ejsLayouts);
+app.use(require('morgan')('dev'))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(ejsLayouts)
 
 // GET - main index of site
 app.get('/', function(req, res) {
-    let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+    let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/'
     // Use request to call the API
     axios.get(pokemonUrl).then(response => {
-        let pokemon = response.data.results;
-        res.render('index', { pokemon: pokemon.slice(0, 151) });
-    });
-});
+        let pokemon = response.data.results
+        res.render('index', { pokemon: pokemon.slice(0, 151) })
+    })
+})
 
 // Imports all routes from the pokemon routes file
-app.use('/pokemon', require('./routes/pokemon'));
+app.use('/pokemon', require('./routes/pokemon'))
 
 const server = app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
-});
+    console.log(`Server is running on ${port}`)
+})
 
-module.exports = server;
+module.exports = server
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,17 +1,20 @@
-var express = require('express');
-var router = express.Router();
-const db = require('./models');
+var express = require('express')
+var router = express.Router()
+const db = require('./models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/pokemon', function(req, res) {
     // TODO: Get all records from the DB and render to view
-    res.render('pokemon/index');
-});
+    res.render('pokemon/index')
+})
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/pokemon', function(req, res) {
     // TODO: Get form data and add a new record to DB
-    res.redirect('/pokemon');
-});
+    db.pokemon.create({
+        name: req.body.name
+    })
+    res.redirect('/pokemon')
+})
 
-module.exports = router;
+module.exports = router
