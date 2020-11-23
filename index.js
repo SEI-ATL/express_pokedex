60f8a6185b976450925e3983514027a5cbcedfb8
Edree Huss <edreeshuss@gmail.com>
2020-11-22 22:26:45 -0500

    [Express_Pokedex] Complete and clean up code. Add styling.

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
+
 
 // GET - main index of site
 app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151/';
   // Use request to call the API
   axios.get(pokemonUrl).then(response => {
     let pokemon = response.data.results;
