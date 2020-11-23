860d2350543fc3e6c2569ca2ea55e63b53607c20
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-11-22 21:33:58 -0500

    Deliverable complete, added addition functions such as links to pokemon details from home page and ability to add to favorite from details page.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
-const db = require('./models');
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
@@ -13,7 +12,7 @@ app.use(ejsLayouts);
 
 // GET - main index of site
 app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then(response => {
     let pokemon = response.data.results;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,17 +1,46 @@
 var express = require('express');
 var router = express.Router();
-const db = require('./models');
+const axios = require('axios'); 
+
+const db = require('../models');
+const { response } = require('express');
+
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(allPokemon => {
+    console.log(allPokemon)
+    
+    res.render('pokemon/index', { pokemon: allPokemon });
+  })
 });
+
+// Get route for pokemon id
+router.get('/:id', (req, res) => {
+  console.log(req.params.id);
+  axios.get(`http://pokeapi.co/api/v2/pokemon/${req.params.id}`).then(response => {
+    // let image = response.data.sprites.other['official-artwork'].front_default
+    console.log(response.data.sprites.other['official-artwork'].front_default);
+    res.render('pokemon/details' , { pokemon: response.data})
+  })
+
+
+
+})
+
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(() => {
+    res.redirect('/pokemon')
+  })
+  
 });
 
 module.exports = router;
