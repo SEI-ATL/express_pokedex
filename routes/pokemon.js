2d02e5ee539b1539624f51cf6871c3e6542d0edc
Will Locklier <will.locklier@gmail.com>
2020-11-22 22:06:03 -0500

    [pokedex] Add routes for favorties + redirect

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models')
+const axios = require('axios')
 
-// GET /pokemon - return a page with favorited Pokemon
+// GET /favorites - return a page with favorited Pokemon
 router.get('/', function(req, res) {
+  db.pokemon.findAll().then (allOfEm => {
+    res.render('favorites', { pokemon: allOfEm})
+  })
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(() => {
+    res.redirect('/pokemon')
+
+  })
 });
+
+
+
+router.get('/:id', (req, res) => {
+  
+
+})
 
 module.exports = router;
