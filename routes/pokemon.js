c8fec19423e13f874a57e99c9738df5a346de97b
jtreeves <jackson.t.reeves@gmail.com>
2020-11-22 23:23:24 -0500

    [routes] Add route to display details about individual pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,18 +3,26 @@ var router = express.Router()
 const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     db.pokemon.findAll().then(pokemons => {
         res.render('pokemon/index', { pokemons })
     })
 })
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/', (req, res) => {
     db.pokemon.create({
         name: req.body.name
     }).then(post => {
         res.redirect('/pokemon')
+    })
+})
+
+router.get('/pokemon/:id', (req, res) => {
+    db.pokemon.findOne({
+        where: { id: req.params.id }
+    }).then(pokemon => {
+        res.render('pokemon/show', { pokemon })
     })
 })
 
